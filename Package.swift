a6a437966902eaac707db3dbb8b8124104f900f7
Aaron Vegh <aaron@innoveghtive.com>
2020-12-08 15:21:18 -0500

    Update project and Package for SPM

--- Package.swift
+++ Package.swift
@@ -1,7 +1,18 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
     name: "SwiftCron",
-    targets: [],
+	products: [
+		// library that has the same name as the package itself:
+        .library(
+            name: "SwiftCron",
+            targets: ["SwiftCron"]
+        )
+	],
+    targets: [
+		.target(name: "SwiftCron")
+	],
     dependencies:[]
 )
